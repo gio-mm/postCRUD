f32c7cc1c26435eda5ea270fdbcab6409dcef753
gio <gionachye15@gmail.com>
2021-03-24 19:02:53 +0400

    create methords and views for comments,update comments routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,33 @@
-class PostsController < ApplicationController
-  def index
+class CommentsController < ApplicationController
+
+  def show
+    @comment=Comment.find(params[:id])
+    @post=@comment.post
 
   end
+
+  def create
+    @comment=Comment.new(params.require(:comments).permit(:post_id,:body))
+    @comment.save
+    redirect_to @comment.post
+  end
+  def edit
+    @comment=Comment.find(params[:id])
+
+  end
+  def update
+
+    @comment=Comment.find(params[:id])
+    @comment.update(params.require(:comment).permit(:body))
+
+    flash[:notice] = "comment was updated successfully"
+    redirect_to @comment
+  end
+  def destroy
+    @comment=Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @comment.post
+  end
+
 end
+
