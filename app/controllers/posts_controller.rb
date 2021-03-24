9a6534435d4806e5145592c2e7bbf629588ab7b9
gio <gionachye15@gmail.com>
2021-03-24 17:21:20 +0400

    create posts#index method and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,35 @@
 class PostsController < ApplicationController
-  def index
 
-  end
+    def show
+
+    end
+
+    def index
+      @posts=Post.where(mark_for_deletion: false).order(created_at: :desc).first(10)
+    end
+
+    def new
+
+    end
+    def create
+
+
+    end
+    def edit
+
+
+
+    end
+
+
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
 end
